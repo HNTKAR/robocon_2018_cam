b6841de922211aea7a9d151ff933a8e394f36256
kusari-k <s15426@salesio-sp.ac.jp>
2019-06-22 16:11:40 +0900

    test pictures

---
+++ pictest1.py
@@ -0,0 +1,24 @@
+#-*- coding:utf-8 -*-
+import time
+import picamera
+import subprocess
+parae=["off","auto","night","nightpreview","backlight","spotlight","sports","snow","beach","verylong","fixedfps","antishake","fireworks"]
+param=["average","spot","backlit","matrix"]
+paraa=["off","auto","sunlight","cloudy","shade","tungsten","fluorescent","incandescent","flash","horizon"]
+with picamera.PiCamera() as picamera:
+    num=0
+    file_name=str(num)
+    for e in parae:
+        for m in param:
+            for a in paraa:
+                file_name=a+"-"+m+"-"+e
+                picamera.awb_mode=a
+                picamera.meter_mode=m
+                picamera.exposure_mode=e
+                #picamera.start_preview()
+                #time.sleep(3)
+                picamera.capture("/home/pi/Desktop/robocon/test1/"+file_name+'.jpg')
+                
+    print ("\n")
+    print("撮影済リスト")
+    res = subprocess.call('ls')
